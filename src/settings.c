ea2e8596acbbc089427bbd66cdb14465cbee713f
indadul-hoque <skindadul4321@gmail.com>
2022-12-05 15:23:38 +0530

    create setting modiul

---
+++ src/settings.c
@@ -0,0 +1,22 @@
+#include "system.h"
+
+int main()
+{
+     int ch;
+     printf("Press 1: For change your password\n");
+     printf("Press 2: For Delete your account\n");
+     printf("Press 3: For Logout from your account\n");
+     while(1)
+     {
+        switch(ch)
+        {
+            case 1: changepass();
+            break;
+            case 2: deletaccount();
+            break;
+            case 3: logout();
+            break;
+         }   
+     }
+}
+
